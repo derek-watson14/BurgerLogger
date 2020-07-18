3b58f841270267f1a8ba67c8309267e0394d3022
Derek Watson <derek.watson92@gmail.com>
2020-07-18 13:11:47 -0700

    working code without unique styling

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('The Burg');
+INSERT INTO burgers (burger_name) VALUES ('The Shroomer');
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Great Pesto Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hangover Helper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('GoJo Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('The Old Fashioned');
